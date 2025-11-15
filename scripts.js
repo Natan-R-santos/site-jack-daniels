8e1e7bdd0b500018b781f5b21cb6282fdf070e1d
Natan Ramos Costa Dos Santos <nathanrcs2022@gmail.com>
2025-11-15 12:14:54 -0300

    Correct JavaScript syntax and functionality in scripts.js
    Fix multiple issues in scripts.js including event listener and class manipulation.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-alert("deu bom")
+alert("deu bom");
+console.log("feito por Natan rcs");
+const menuMiboles = document.getElementById("menuMobiles");
+const menuBtn =document.getElementById("menuBtn");
+menuBtn.addEventlistener("click", () =>{
+  menuMobiles.classlist.toggle("active");});
+const images = document.querySELECTORALL(".conteiner div");
+const mover = 0;
+function moverDiv(){
+  images.forEach(img=>img.classlist.remove("active"));
+  images[index].classlist.add("active");
+  index++;
+  if (index => images.length){index=0;}} setInterval(mudarDiv,3500);
+
